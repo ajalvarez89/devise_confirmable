18584f0d777880b487807f5f30a61e5cac82185a
Alvaro Alvarez <alvaro.alvarez@2transfair.com>
2020-02-13 09:31:58 -0500

    setting styles for view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     confirmations: 'confirmations'
   }
-  resources :home, only: [:index]
   resources :dashboard, only: [:index]
   root to: "dashboard#index"
 end
